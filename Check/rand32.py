decb712aa6cfb373f85d54b1cbd49524851bd759
Pietro Inglese <magicenglish93@gmail.com>
2018-03-07 10:40:54 +0100

    Add of check (sum, add and div) and random 32 bit generator

---
+++ Check/rand32.py
@@ -0,0 +1,12 @@
+from random import *
+import argparse
+
+# The argument should be the number of lines in the output
+parser = argparse.ArgumentParser(description='Choice of output random numbers')
+parser.add_argument('numOfLines', nargs='?', default = 200, type = int)
+args = parser.parse_args()
+
+with open('inputs', 'w') as f:
+    for i in range (0, args.numOfLines - 1):
+        f.write('{0:032b}\n'.format(getrandbits(32)))
+
